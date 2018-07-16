3885ea1134863fa5550aa9e86ba8e8f19e6b4122
Sneakzz <sneakygamer94@gmail.com>
2018-07-16 20:19:55 +0200

    Removed last for loop in the view.displayTodos method and replaced with forEach method.

--- Version 8/Todolist-V8.js
+++ Version 8/Todolist-V8.js
@@ -74,9 +74,11 @@ var view = {
   displayTodos: function() {
     var todosUl = document.querySelector('ul');
     todosUl.innerHTML = '';
-    for (var i = 0; i < todoList.todos.length; i++) {
+
+    // this // refers to the view object
+    // forEach(callback, this);
+    todoList.todos.forEach(function(todo, position) {
       var todoLi = document.createElement('li');
-      var todo = todoList.todos[i];
       var todoTextWithCompletion = '';
 
       if (todo.completed === true) {
@@ -85,11 +87,11 @@ var view = {
         todoTextWithCompletion = '( ) ' + todo.todoText;
       }
 
-      todoLi.id = i;
+      todoLi.id = position;
       todoLi.textContent = todoTextWithCompletion;
       todoLi.appendChild(this.createDeleteButton());
       todosUl.appendChild(todoLi);
-    }
+    }, this);
   },
   createDeleteButton: function() {
     var deleteButton = document.createElement('button');
